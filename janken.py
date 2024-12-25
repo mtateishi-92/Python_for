c3fe065d3924b370e578c155ed84efb9f2356da2
Mutsumi Tateishi <AU988981@jp.ibm.com>
2024-12-25 15:37:19 +0900

    処理記述

--- janken.py
+++ janken.py
@@ -1,4 +1,19 @@
 import random
 
-print('0：グー、1：チョキ、3：パー')
-input('じゃんけんを始めます。数字を入力してください：')
+print('じゃんけんを始めます。')
+player_input = input('数字を入力してください（0：グー、1：チョキ、3：パー）：')
+lists = {0:'グー', 1:'チョキ', 2:'パー'}
+
+int(player_input)
+
+player = lists[player_input]
+com = random.choice(lists)
+print(player)
+#print(com)
+for com_list in lists:
+    if 'グー' == 'グー':
+        print('COMは'+ com +'をだしました。')
+    if 'チョキ' == 'チョキ':
+        print('COMは'+ com +'をだしました。')
+    if 'パー' == 'パー':
+        print('COMは'+ com +'をだしました。')
