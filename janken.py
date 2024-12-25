2cdfea91f93a751ce7201907f539d7b8b2ecb7a7
Mutsumi Tateishi <AU988981@jp.ibm.com>
2024-12-25 16:24:00 +0900

    じゃんけん完成

--- janken.py
+++ janken.py
@@ -1,19 +1,32 @@
 import random
 
 print('じゃんけんを始めます。')
-player_input = input('数字を入力してください（0：グー、1：チョキ、3：パー）：')
-lists = {0:'グー', 1:'チョキ', 2:'パー'}
+player_input = input('数字を入力してください（0：グー、1：チョキ、2：パー）：')
+lists = ['グー', 'チョキ', 'パー']
 
-int(player_input)
-
-player = lists[player_input]
+player = lists[int(player_input)]
 com = random.choice(lists)
-print(player)
-#print(com)
-for com_list in lists:
-    if 'グー' == 'グー':
-        print('COMは'+ com +'をだしました。')
-    if 'チョキ' == 'チョキ':
-        print('COMは'+ com +'をだしました。')
-    if 'パー' == 'パー':
-        print('COMは'+ com +'をだしました。')
+
+print('あなたは' + player + 'をだしました。')
+print('COMは'+ com +'をだしました。')
+
+if player == 'グー' and com == 'グー':
+    print('引き分けです。')
+if player == 'チョキ' and com == 'チョキ':
+    print('引き分けです。')
+if player == 'パー' and com == 'パー':
+    print('引き分けです。')
+
+if player == 'グー' and com == 'チョキ':
+    print('あなたの勝ちです。')
+if player == 'チョキ' and com == 'パー':
+    print('あなたの勝ちです。')
+if player == 'パー' and com == 'グー':
+    print('あなたの勝ちです。')
+
+if player == 'グー' and com == 'パー':
+    print('あなたの負けです。')
+if player == 'チョキ' and com == 'グー':
+    print('あなたの負けです。')
+if player == 'パー' and com == 'チョキ':
+    print('あなたの負けです。')
